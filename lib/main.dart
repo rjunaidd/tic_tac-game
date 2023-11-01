0041d5431af7a57902da37d07f13b4c84e204ac9
rjunaidd <roafjunaid09@gmail.com>
2023-11-01 10:09:44 -0700

    tic

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:tic_tac/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp( ));
 }
 
 class MyApp extends StatelessWidget {
